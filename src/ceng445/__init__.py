b907e47eea22c480a830263c6e9e57e816e06928
mgencol <e1881226@ceng.metu.edu.tr>
2017-01-04 22:24:26 +0300

    Clean old ceng445 directory

--- src/ceng445/__init__.py
+++
@@ -1,6 +0,0 @@
-from .Application import Application
-from .Database import Database
-from . import components
-from .Factory import Factory
-
-__all__ = ['Application', 'components', 'Database', 'Factory']
