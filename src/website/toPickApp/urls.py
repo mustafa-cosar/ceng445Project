b726c7fddfd7e109e1de142192ca8bc5a2dfdb96
Mustafa Senol Cosar <e1881135@ceng.metu.edu.tr>
2017-01-06 05:17:18 +0300

    Implemented loadComponent

--- src/website/toPickApp/urls.py
+++ src/website/toPickApp/urls.py
@@ -5,7 +5,7 @@ from toPickApp.views import BaseClass
 urlpatterns = [
     url(r'^$', BaseClass.as_view()),
     url(r'^load$', BaseClass.as_view()),
-    url(r'^loadComponent$', BaseClass.as_view()),
+    url(r'^loadComponent$', BaseClass.as_view(name='loadComponent')),
     url(r'^addInstance$', BaseClass.as_view()),
     url(r'^removeInstance$', BaseClass.as_view()),
     url(r'^execute$', BaseClass.as_view()),
