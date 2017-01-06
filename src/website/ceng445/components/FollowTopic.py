9ab852ce4f17115a149a48c822990f654d641805
Mustafa Senol Cosar <e1881135@ceng.metu.edu.tr>
2017-01-06 19:06:10 +0300

    Added FollowTopic, GetPosts, Post, UnfollowTopic component skeletons

---
+++ src/website/ceng445/components/FollowTopic.py
@@ -0,0 +1,15 @@
+from .Component import Component
+
+class FollowTopic(Component):
+    def __init__(self):
+
+
+    def __str__(self):
+        return 'Follow Topic'
+
+    def description(self):
+        return 'User follows a Topic'
+
+    def execute(self):
+        #TODO Implement FollowTopic
+        return ''
